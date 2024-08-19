3bb9bcc9a119a43022e67d215336c1cc5882a3b2
Ceyda Turkdogan <ceyda.turkdogan@gmail.com>
2024-08-19 23:26:15 +0300

    Add project files.

---
+++ ArrayExample/Program.cs
@@ -0,0 +1,25 @@
+﻿
+int[] array= new int[10];
+
+for (int i = 0;i< array.Length; i++)
+{
+    array[i] = i;
+}
+
+foreach (int item in array)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine("Diziye eklemek istediğiniz tam sayıyı giriniz");
+
+int lastElement=Convert.ToInt32(Console.ReadLine());
+
+Array.Resize(ref array, array.Length+1);
+
+array[array.Length-1] = lastElement;
+
+//int maxNumber = array[0];
+
+
+
